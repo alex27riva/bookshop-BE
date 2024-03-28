eee4d26cec8989f0e0af0c8518fc2328ef2d21a3
Alessandro Riva <alex27riva@gmail.com>
2024-03-28 17:07:15 +0100

    Validate token from pubkey

--- app/keycloak_validator.py
+++ app/keycloak_validator.py
@@ -7,6 +7,8 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
+target_kid = 'mqp6ifE0E7ArXObW_1Cpshk8lwktc3Wklhk8UDlypPE'
+
 
 class KeycloakValidator:
     def __init__(self, certs_url, client_id):
@@ -18,8 +20,16 @@ class KeycloakValidator:
     def _get_public_key(self):
         response = requests.get(self.certs_url)
         keys = response.json()["keys"]
-        for key in keys:
-            if key["kid"]:
-                return jwt.algorithms.RSAAlgorithm.from_jwk(key)
+        print(keys)
+        selected_key = next((key for key in keys if key['kid'] == target_kid), None)
+        if selected_key:
+            logging.debug(f"Selected key: {selected_key}")
+            return selected_key
         return None
 
+    def validate_token(self, token):
+        try:
+            decoded_token = jwt.decode(token, self.public_key, algorithms=['RS256'])
+            print(decoded_token)
+        except Exception as e:
+            logging.error(f"Error decoding token: {e}")
