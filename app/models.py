cd48bfbaffb4181d672e8b30e59e409b0cc6e5d6
Alessandro Riva <alex27riva@gmail.com>
2024-03-31 11:09:40 +0200

    Removed flask-security from models.py

--- app/models.py
+++ app/models.py
@@ -1,31 +1,20 @@
-from flask_security import RoleMixin, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
-# Define the User and Role models for Flask-Security
-class Role(db.Model, RoleMixin):
+class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean)
-    roles = db.relationship('Role', secondary='user_roles')
-    fs_uniquifier = db.Column(db.String(64), unique=True)
-
-
-# Define the UserRoles model for the many-to-many relationship between users and roles
-class UserRoles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
 
 # Define the Book model
