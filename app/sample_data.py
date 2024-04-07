c611621ab971cfcbad696f91827dcf4f110f19fa
Alessandro Riva <alex27riva@gmail.com>
2024-04-07 11:15:15 +0200

    Add book

--- app/sample_data.py
+++ app/sample_data.py
@@ -2,4 +2,6 @@ book_data = [
     ('1984', 'George Orwell', 9.99, 'https://www.bookerworm.com/images/1984.jpg'),
     ('Brave New World', 'Aldous Huxley', 12.99,
      'https://upload.wikimedia.org/wikipedia/en/6/62/BraveNewWorld_FirstEdition.jpg'),
+    ('Thinking, Fast and Slow', 'Daniel Kahneman', 13.49, 'https://upload.wikimedia.org/wikipedia/en/c/c1/Thinking'
+                                                          '%2C_Fast_and_Slow.jpg')
 ]
