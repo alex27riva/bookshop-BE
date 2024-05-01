9b74488f3b3f4c4dc3d23100f33222b9ff5b78ed
Alessandro Riva <alex27riva@gmail.com>
2024-05-01 21:37:08 +0200

    Add books

--- app/sample_data.py
+++ app/sample_data.py
@@ -1,7 +1,10 @@
 book_data = [
-    ('1984', 'George Orwell', 9.99, 'https://www.bookerworm.com/images/1984.jpg'),
+    ('1984', 'George Orwell', 9.99, 'https://i.postimg.cc/BQKVQtxh/1984.jpg'),
     ('Brave New World', 'Aldous Huxley', 12.99,
      'https://upload.wikimedia.org/wikipedia/en/6/62/BraveNewWorld_FirstEdition.jpg'),
     ('Thinking, Fast and Slow', 'Daniel Kahneman', 13.49, 'https://upload.wikimedia.org/wikipedia/en/c/c1/Thinking'
-                                                          '%2C_Fast_and_Slow.jpg')
+                                                          '%2C_Fast_and_Slow.jpg'),
+    ('Hacking: The Art of Exploitation', 'Jon Erickson', 34.99, 'https://i.postimg.cc/C5dgdYFF/Hacking-The-Art-of'
+                                                                '-Exploitation.jpg'),
+    ('Fahrenheit 451', 'Ray Bradbury', 7.99, 'https://i.postimg.cc/QxbQj3FB/farenheit-451.jpg')
 ]
